21f9071477dc38c8a7186fc803e12ac91b6040b5
hazzawinchester <winchesterhj@gmail.com>
2025-03-25 20:29:46 +0000

    WHA

--- challenge/views/stego.py
+++ challenge/views/stego.py
@@ -11,8 +11,15 @@ class Challenge2View(views.View):
 app.add_url_rule("/2/", view_func=Challenge2View.as_view("challenge2"))
 
 class Challenge1View(views.View):
-    template_name = "challenge1.html"
+    template_name = "challenge4.html"
 
     def dispatch_request(self) -> str:
         return render_template(self.template_name)
 app.add_url_rule("/1/", view_func=Challenge1View.as_view("challenge1"))
+
+class Challenge4View(views.View):
+    template_name = "challenge4.html"
+
+    def dispatch_request(self) -> str:
+        return render_template(self.template_name)
+app.add_url_rule("/4/", view_func=Challenge1View.as_view("challenge4"))
