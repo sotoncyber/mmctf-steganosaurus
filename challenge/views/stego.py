3d8d0e5e4fddafa1937209a5098f1d213c518c58
tombash766 <87862533+tombash766@users.noreply.github.com>
2025-03-25 13:12:03 +0000

    massive changes

--- challenge/views/stego.py
+++ challenge/views/stego.py
@@ -8,13 +8,11 @@ class Challenge2View(views.View):
 
     def dispatch_request(self) -> str:
         return render_template(self.template_name)
+app.add_url_rule("/2/", view_func=Challenge2View.as_view("challenge2"))
 
 class Challenge1View(views.View):
     template_name = "challenge1.html"
 
     def dispatch_request(self) -> str:
         return render_template(self.template_name)
-
-
-app.add_url_rule("/2/", view_func=Challenge2View.as_view("challenge2"))
 app.add_url_rule("/1/", view_func=Challenge1View.as_view("challenge1"))
